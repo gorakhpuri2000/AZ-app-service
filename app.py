f6bfeef5f788c93cc3434003a82678a57c8e477e
gorakhpuri2000 <gorakhpuri2000@gmail.com>
2026-01-02 15:15:01 +0530

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,18 @@ app = Flask(__name__)
 
 @app.route("/")
 def home():
-    return "Hello everyone from Azure App Service 🚀"
+    return """
+    <html>
+        <head>
+            <title>My Python Web Page</title>
+        </head>
+        <body style="font-family: Arial; background-color:#f4f4f4; text-align:center;">
+            <h1>Welcome to My Python Web Page 🚀</h1>
+            <p>This page is running using Flask</p>
+            <p>Deployed on Azure App Service</p>
+        </body>
+    </html>
+    """
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=8000)
